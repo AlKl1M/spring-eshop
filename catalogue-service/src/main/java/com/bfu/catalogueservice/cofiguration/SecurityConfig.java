5e902566cf7c6d91dea5bb1afbc625e155a3b0ef
alkl1m <al.klimkovetskiy@gmail.com>
2024-04-11 17:51:01 +0200

    For now, the gateway has been removed. Changed the security concept.

--- catalogue-service/src/main/java/com/bfu/catalogueservice/cofiguration/SecurityConfig.java
+++ catalogue-service/src/main/java/com/bfu/catalogueservice/cofiguration/SecurityConfig.java
@@ -5,51 +5,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
 import java.util.stream.Stream;
 
+
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-                        .requestMatchers(HttpMethod.POST, "/api/catalogue/**")
-                        .hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/api/catalogue**")
-                        .hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/catalogue/**")
-                        .hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.GET).hasAnyRole("MANAGER", "CUSTOMER")
-                        .anyRequest().denyAll())
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+                .authorizeHttpRequests(configuration -> configuration
+                        .requestMatchers(HttpMethod.POST, "/api/catalogue/")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.PUT, "/api/catalogue/")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.DELETE, "/api/catalogue/")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.GET, "/api/catalogue/")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .anyRequest().authenticated())
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(CsrfConfigurer::disable)
+                .oauth2ResourceServer(configuration -> configuration.jwt(Customizer.withDefaults()))
                 .build();
-    }
-
-    @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        var converter = new JwtAuthenticationConverter();
-        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        converter.setPrincipalClaimName("preferred_username");
-        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
-            var authorities = jwtGrantedAuthoritiesConverter.convert(jwt);
-            var roles = (List<String>)jwt.getClaimAsMap("realm_access").get("roles");
-
-            return Stream.concat(authorities.stream(),
-                    roles.stream()
-                            .filter(role -> role.startsWith("ROLE_"))
-                            .map(SimpleGrantedAuthority::new)
-                            .map(GrantedAuthority.class::cast))
-                    .toList();
-        });
-        return converter;
     }
 }
