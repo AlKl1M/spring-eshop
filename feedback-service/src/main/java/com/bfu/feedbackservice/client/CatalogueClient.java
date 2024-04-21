40cfab9d2f9101f5b1185acf7bfbd589708e085f
alkl1m <al.klimkovetskiy@gmail.com>
2024-04-21 20:27:12 +0200

    Discovery config for feedback added

--- feedback-service/src/main/java/com/bfu/feedbackservice/client/CatalogueClient.java
+++ feedback-service/src/main/java/com/bfu/feedbackservice/client/CatalogueClient.java
@@ -1,4 +1,7 @@
 package com.bfu.feedbackservice.client;
 
+import com.bfu.feedbackservice.controller.payload.SimplifiedProductResponse;
+
 public interface CatalogueClient {
+    SimplifiedProductResponse getProductInfo(String productId);
 }
--- feedback-service/src/main/java/com/bfu/feedbackservice/config/SecurityConfig.java
+++ feedback-service/src/main/java/com/bfu/feedbackservice/config/SecurityConfig.java
@@ -5,17 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(authorize -> authorize
-                .anyRequest().authenticated())
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-        return httpSecurity.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                        .anyRequest().authenticated())
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2Client(Customizer.withDefaults());
+        return http.build();
     }
 }
