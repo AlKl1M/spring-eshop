1ffefb03c9dd72f12ace2be1b9ced41c82362336
alkl1m <al.klimkovetskiy@gmail.com>
2024-04-25 19:37:45 +0200

    catalogue service removed from cart service branch

--- catalogue-service/src/main/java/com/bfu/catalogueservice/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bfu.catalogueservice.repository;
-
-import com.bfu.catalogueservice.entity.Brand;
-import com.bfu.catalogueservice.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByCategoryId(String categoryId);
-}
--- catalogue-service/src/main/java/com/bfu/catalogueservice/repository/ProductPhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bfu.catalogueservice.repository;
-
-import com.bfu.catalogueservice.entity.ProductPhoto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductPhotoRepository extends JpaRepository<ProductPhoto, Long> {
-}
