3831c946ffcea6bbfb58275e81c8a2ac0f28ccf8
alkl1m <al.klimkovetskiy@gmail.com>
2024-04-27 11:27:36 +0200

    Integrational tests for cart and refactoring

--- cart-service/src/main/java/com/bfu/cartservice/controller/payload/AddProductToCartPayload.java
+++ cart-service/src/main/java/com/bfu/cartservice/controller/payload/AddProductToCartPayload.java
@@ -4,9 +4,9 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record AddProductToCartPayload(
-        @NotNull(message="cart.add.errors.product_id_is_null")
+        @NotNull(message="{cart.add.errors.product_id_is_null}")
         String productId,
-        @Min(value = 1, message = "cart.add.quantity_is_below_min")
+        @Min(value = 1, message = "{cart.add.quantity_is_below_min}")
         Integer quantity
 ) {
 }
