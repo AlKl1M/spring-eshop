2c1bf61f325dfd9d431a775a18d8bbee5fc2a8fa
Alex Klimkovetskiy <88432319+AlKl1M@users.noreply.github.com>
2024-04-29 19:04:08 +0200

    Delete catalogue-service directory

--- catalogue-service/src/main/java/com/bfu/catalogueservice/service/product_photo/ProductPhotoService.java
+++
@@ -1,8 +0,0 @@
-package com.bfu.catalogueservice.service.product_photo;
-
-import com.bfu.catalogueservice.controller.payload.ProductPhoto.CreateProductPhotoResponse;
-import java.io.IOException;
-
-public interface ProductPhotoService {
-    void createProductPhoto(CreateProductPhotoResponse productPhotoResponse) throws IOException;
-}
